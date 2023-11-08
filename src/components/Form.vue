8eecb8c89860082d4ce343ba4e1621f27ce597c5
Luke Taylor <luke@circleleasing.co.uk>
2023-11-08 16:19:31 +0000

    Create form component

---
+++ src/components/Form.vue
@@ -0,0 +1,29 @@
+<template>
+    <form :id="id" @submit.prevent="handleSubmit">
+        <Card>
+            <template #content>
+                <slot></slot>
+
+                <div class="text-center mt-3">
+                    <Button type="submit">Submit</Button>
+                </div>
+            </template>
+        </Card>
+    </form>
+</template>
+
+<script setup lang="ts">
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+
+const emit = defineEmits(['submit']);
+
+const id = crypto.randomUUID();
+
+function handleSubmit(e: Event) {
+    const form = document.getElementById(id) as HTMLFormElement;
+    if(form.checkValidity()){
+        emit('submit');
+    }
+}
+</script>
