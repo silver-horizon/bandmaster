4448bed2c7399c4304eb6425a1cd75d0ebdd8045
Luke Taylor <luke@circleleasing.co.uk>
2023-11-17 12:23:28 +0000

    Remove concept of GroupUser

--- src/service/GroupService.ts
+++ src/service/GroupService.ts
@@ -1,5 +1,6 @@
-import type { IGroup, IMember } from "../../../bandmaster-common/type/Groups";
-import type {ICreateMemberDto, ICreateGroupDto, IMoveSectionDto} from "@/type/Dto";
+import type { IGroup } from "../../../bandmaster-common/type/Groups";
+import type { IUser } from "../../../bandmaster-common/type/Users";
+import type {ICreateMemberDto, ICreateGroupDto, IMoveSectionDto} from "../../../bandmaster-common/type/Dto";
 import { dateToString, getFromApi, postToApi } from "@/utils";
 
 const getApiUrl = (endpoint: string = '') => `${import.meta.env.VITE_API_URL}/groups${endpoint}`;
@@ -17,7 +18,7 @@ export default {
         return await postToApi(getApiUrl('/add'), groupData);
     },
 
-    async addMemberToGroup(groupId: string, sectionId: string, memberData: ICreateMemberDto, id: string | null = null): Promise<IMember> {
+    async addMemberToGroup(groupId: string, sectionId: string, memberData: ICreateMemberDto, id: string | null = null): Promise<IUser> {
 
         const payload: {firstName: string, lastName: string, email: string, dob: string, contact?: any, sectionId: string} = {
             ...memberData,
@@ -41,7 +42,7 @@ export default {
         return await postToApi(getApiUrl(`/${groupId}/members/${userId}/section`), sectionData);
     },
 
-    async getUserInGroupById(groupId: string, userId: string): Promise<IMember>{
+    async getUserInGroupById(groupId: string, userId: string): Promise<IUser>{
         const result = await getFromApi(getApiUrl(`/${groupId}/members/${userId}`));    
         if(result){
             result.dob = new Date(result.dob);
