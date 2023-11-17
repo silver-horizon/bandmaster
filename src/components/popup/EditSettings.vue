07db84a57d18b3af2462bd824e1038e2034caf71
Luke Taylor <luke@circleleasing.co.uk>
2023-11-17 15:47:14 +0000

    Add preference for creating users

--- src/components/popup/EditSettings.vue
+++ src/components/popup/EditSettings.vue
@@ -1,10 +1,17 @@
 <template>
-    <label class="block" for="allowEdit">Allow my groups to change my details</label>
-    <InputSwitch input-id="allowEdit" v-model="modelValue.allowEdit" @change="update"></InputSwitch>
+    <div class="mb-5">
+        <label class="block mb-3" for="allowEdit">Allow my groups to change my details</label>
+        <InputSwitch input-id="allowEdit" v-model="modelValue.allowEdit" @change="update"></InputSwitch>
+    </div>
+
+    <div>
+        <label class="block mb-3" for="allowCreate">Allow groups to add new users to my account</label>
+        <InputSwitch input-id="allowCreate" v-model="modelValue.allowCreate" @change="update"></InputSwitch>
+    </div>
 </template>
 
 <script setup lang="ts">
-import type {IPreferences} from "../../../../bandmaster-common/type/Users";
+import type { IPreferences } from "../../../../bandmaster-common/type/Users";
 
 import InputSwitch from 'primevue/inputswitch';
 
@@ -13,7 +20,7 @@ const { modelValue } = defineProps<{
 }>();
 const emit = defineEmits(['update:modelValue']);
 
-function update(){
+function update() {
     emit('update:modelValue', modelValue);
 }
 </script>
