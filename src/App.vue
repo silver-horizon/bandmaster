8b5ea650a5b6bcf16fdf20e53dc1b1379e359d4f
Luke Taylor <luke@circleleasing.co.uk>
2023-12-07 16:44:28 +0000

    Move existing links to accordion - closes #16

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,34 @@
 <style scoped>
+:deep(.p-accordion-content){
+  background-color: rgba(255, 255, 255, 0.15);
+  border-color: rgba(255,255,255,0.1);
+  padding: 0.5rem;
+}
+
+:deep(.p-accordion-header-link){
+  transition: border-color .5s, background-color .5s;
+  background-color: transparent;
+  border-color: transparent;
+  color: var(--primary-color-text);
+  padding-left: 0.25rem;
+}
+
+:deep(.p-accordion-header.p-highlight .p-accordion-header-link){
+  background-color: rgba(0,0,0,0.15);
+  border-bottom: solid 1px rgba(255,255,255,0.5);
+}
+
+:deep(.p-accordion-tab){
+  border: 1px solid;
+  border-radius: var(--border-radius);
+  border-color: transparent;
+  transition: border-color .5s;
+}
+
+:deep(.p-accordion-tab-active){
+  border-color: rgba(255,255,255,0.5);
+}
+
 header {
   background: var(--primary-color);
   color: var(--primary-color-text);
@@ -112,10 +142,11 @@ ul {
     flex-direction: column;
     height: calc(100% - 100px);
     overflow: auto;
+    padding-right: 1rem;
   }
 
   .navbar {
-    max-width: 80%;
+    max-width: 90%;
     max-height: none;
     margin-left: auto;
     margin-right: auto;
@@ -170,20 +201,20 @@ ul {
           <Dropdown class="w-full" :options="availableGroups" option-label="name" option-value="id" v-model="id" @change="changeGroup"></Dropdown>
           <label>Group</label>
         </div>
-        <ul class="mb-auto">
-          <li>
-            <RouterLink :to="{ name: 'memberList' }" class="large" @click="toggleNav(false)">Members</RouterLink>
-          </li>
-          <li>
-            <RouterLink :to="{ name: 'listSections' }" class="large" @click="toggleNav(false)">Sections</RouterLink>
-          </li>
-        </ul>
+
+        <Accordion :multiple="true" class="large">
+          <AccordionTab v-for="item in navMenu" :header="item.name">
+            <ul class="ml-3">
+              <li v-for="page in item.pages"><RouterLink :to="{name: page.link}">{{ page.title }}</RouterLink></li>
+            </ul>
+          </AccordionTab>
+        </Accordion>
 
         <hr class="hidden md:block" />
 
         <ul>
           <li>
-            <RouterLink :to="{name: 'myProfile'}" class="large">My Details</RouterLink>
+            <RouterLink :to="{ name: 'myProfile' }" class="large">My Details</RouterLink>
           </li>
           <li>
             <RouterLink to="/" class="large">Sign Out</RouterLink>
@@ -206,6 +237,9 @@ import { useSessionStore } from './stores/SessionStore';
 
 import Dropdown from 'primevue/dropdown';
 import Loader from '@/components/Loader.vue';
+
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 
 const router = useRouter();
 const route = useRoute();
@@ -243,7 +277,7 @@ function toggleNav(forceValue: boolean | null = null) {
 function changeGroup() {
   if (id.value !== "ADD") {
     const match = store.groups.find(x => x.id === id.value);
-    if(match){
+    if (match) {
       store.currentGroup = match;
     }
 
@@ -255,4 +289,15 @@ function changeGroup() {
   router.push({ name: "addGroup" });
   toggleNav(false);
 }
+
+const navMenu = [{
+  name: "Members",
+  pages: [{
+    title: "Personal Details",
+    link: "memberList"
+  }, {
+    title: "Sections",
+    link: "listSections"
+  }]
+}];
 </script>
